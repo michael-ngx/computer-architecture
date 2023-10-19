59df86c2f694db061201f1f1efbd2d0f9b4afb63
michael-ngx <nguyenvietminh812@gmail.com>
2023-10-18 23:01:13 -0400

    fixed 2 level predictor

--- cbp4-assign2/predictor.cc
+++ cbp4-assign2/predictor.cc
@@ -7,7 +7,8 @@ static const UINT32 MAX_VALUE = 3; // 0: NN, 1: NT, 2: TN, 3: TT
 /////////////////////////////////////////////////////////////
 static UINT32 twobitsat_prediction_table[4096] = {0};
 
-void InitPredictor_2bitsat() {}
+void InitPredictor_2bitsat() {
+}
 
 bool GetPrediction_2bitsat(UINT32 PC) {
   UINT32 prediction = twobitsat_prediction_table[PC & 0xFFF];
@@ -30,21 +31,14 @@ void UpdatePredictor_2bitsat(UINT32 PC, bool resolveDir, bool predDir, UINT32 br
 /////////////////////////////////////////////////////////////
 // 2level
 /////////////////////////////////////////////////////////////
-static const INT32 BHT_BITS = 9;
-static const INT32 BHT_WIDTH = 6;
-static const INT32 BHT_ROW_COUNT = 512;
-static UINT32 BHT[BHT_ROW_COUNT] = {0};
-
-static const INT32 PHT_BITS = 3;
-static const INT32 PHT_TABLE_COUNT = 8;
-static const INT32 PHT_ROW_COUNT = 64;
-static UINT32 PHT[PHT_TABLE_COUNT][PHT_ROW_COUNT] = {0};
+static UINT32 BHT[512] = {0};
+static UINT32 PHT[8][64] = {0};
 
 void InitPredictor_2level() {
 }
 
 bool GetPrediction_2level(UINT32 PC) {
-  UINT32 history = BHT[(PC & 0x1FF) >> PHT_BITS] & 0x3F;
+  UINT32 history = BHT[(PC & 0xFF8) >> 3] & 0x3F;
   UINT32 table = PC & 0b111;
   UINT32 prediction = PHT[table][history];
   if (prediction <= 1) return NOT_TAKEN;
@@ -52,15 +46,16 @@ bool GetPrediction_2level(UINT32 PC) {
 }
 
 void UpdatePredictor_2level(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
-  UINT32 history = BHT[(PC & 0x1FF) >> PHT_BITS] & 0x3F;
+  UINT32 history = BHT[(PC & 0xFF8) >> 3] & 0x3F;
   UINT32 table = PC & 0b111;
+  UINT32 predicted = PHT[table][history];
   if (resolveDir == TAKEN) {
-    PHT[table][history] = SatIncrement(PHT[table][history], MAX_VALUE);
-    BHT[(PC & 0x1FF) >> PHT_BITS] = (history << 1) & 1;
+    PHT[table][history] = SatIncrement(predicted, MAX_VALUE);
+    BHT[(PC & 0xFF8) >> 3] = (history << 1) | 1;
   }
   else {
-    PHT[table][history] = SatDecrement(PHT[table][history]);
-    BHT[(PC & 0x1FF) >> PHT_BITS] = (history << 1);
+    PHT[table][history] = SatDecrement(predicted);
+    BHT[(PC & 0xFF8) >> 3] = (history << 1);
   }
 }
 
