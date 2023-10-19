53c2b47923de00bd419b75f4f5b06ba9cd18a8ca
michael-ngx <nguyenvietminh812@gmail.com>
2023-10-18 22:14:55 -0400

    simplified 2bitsat

--- cbp4-assign2/predictor.cc
+++ cbp4-assign2/predictor.cc
@@ -1,25 +1,22 @@
 #include "predictor.h"
 
+static const UINT32 MAX_VALUE = 3; // 0: NN, 1: NT, 2: TN, 3: TT
+
 /////////////////////////////////////////////////////////////
 // 2bitsat
 /////////////////////////////////////////////////////////////
-static const UINT32 MAX_VALUE = 3; // 0: NN, 1: NT, 2: TN, 3: TT
-
-// (8192 bits) / (UINT32 per entry) = 256 entries (8 bits)
-// (UINT32 per entry / 2 bits per prediction) = 16 2bit predictions per entry (4 bits)
-static const PC_BITS = 12;
-static UINT32 2bitsat_prediction_table[256] = {0};
+static UINT32 twobitsat_prediction_table[4096] = {0};
 
 void InitPredictor_2bitsat() {}
 
 bool GetPrediction_2bitsat(UINT32 PC) {
-  UINT32 prediction = 2bitsat_prediction_table[(PC & 0xFF0) >> 4] & (0b11 << (2 * (PC & 0xF)));
+  UINT32 prediction = twobitsat_prediction_table[PC & 0xFFF];
   if (prediction <= 1) return NOT_TAKEN;
   else return TAKEN;
 }
 
 void UpdatePredictor_2bitsat(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
-  UINT32 predicted = 2bitsat_prediction_table[(PC & 0xFF0) >> 4] & (0b11 << (2 * (PC & 0xF)));
+  UINT32 predicted = twobitsat_prediction_table[PC & 0xFFF];
   UINT32 updated;
   if (resolveDir == TAKEN) {
     updated = SatIncrement(predicted, MAX_VALUE);
@@ -27,7 +24,7 @@ void UpdatePredictor_2bitsat(UINT32 PC, bool resolveDir, bool predDir, UINT32 br
   else {
     updated = SatDecrement(predicted);
   }
-  2bitsat_prediction_table[(PC & 0xFF0) >> 4] |= (updated << (2 * (PC & 0xF)));
+  twobitsat_prediction_table[PC & 0xFFF] = updated;
 }
 
 /////////////////////////////////////////////////////////////
@@ -36,22 +33,35 @@ void UpdatePredictor_2bitsat(UINT32 PC, bool resolveDir, bool predDir, UINT32 br
 static const INT32 BHT_BITS = 9;
 static const INT32 BHT_WIDTH = 6;
 static const INT32 BHT_ROW_COUNT = 512;
+static UINT32 BHT[BHT_ROW_COUNT] = {0};
 
 static const INT32 PHT_BITS = 3;
 static const INT32 PHT_TABLE_COUNT = 8;
 static const INT32 PHT_ROW_COUNT = 64;
+static UINT32 PHT[PHT_TABLE_COUNT][PHT_ROW_COUNT] = {0};
 
 void InitPredictor_2level() {
-
 }
 
 bool GetPrediction_2level(UINT32 PC) {
-
-  return TAKEN;
+  UINT32 history = BHT[(PC & 0x1FF) >> PHT_BITS] & 0x3F;
+  UINT32 table = PC & 0b111;
+  UINT32 prediction = PHT[table][history];
+  if (prediction <= 1) return NOT_TAKEN;
+  else return TAKEN;
 }
 
 void UpdatePredictor_2level(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
-
+  UINT32 history = BHT[(PC & 0x1FF) >> PHT_BITS] & 0x3F;
+  UINT32 table = PC & 0b111;
+  if (resolveDir == TAKEN) {
+    PHT[table][history] = SatIncrement(PHT[table][history], MAX_VALUE);
+    BHT[(PC & 0x1FF) >> PHT_BITS] = (history << 1) & 1;
+  }
+  else {
+    PHT[table][history] = SatDecrement(PHT[table][history]);
+    BHT[(PC & 0x1FF) >> PHT_BITS] = (history << 1);
+  }
 }
 
 /////////////////////////////////////////////////////////////
