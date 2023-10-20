710142436f1d8f22897ef6728b5c693dc7fd3f66
michael-ngx <nguyenvietminh812@gmail.com>
2023-10-20 14:35:29 -0400

    added expected outcomes for mb

--- cbp4-assign2/mb.c
+++ cbp4-assign2/mb.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 
-int main() { 
-    int a, b, c, i;                
-    for (i = 0; i < 1000000; i++) { 
-        if (i % 4)  a = 10;
-        if (i % 7)  a = 11;
-        a = 12;
+int main() {
+    int result = 0;
+
+    // Loop a significant number of times to stress-test the predictor.
+    // The expected branch outcome would be:
+    // T - (then inner outcomes) * (1000000 / 3) - N
+    for (int i = 0; i < 1000000; i++) {
+        // Nested loops can create more complex branch patterns.
+        // The expected branch outcome is (inner outcomes)(for-if outcome pairs):
+        // Part 1) i % 3 == 0: TT TN TN TT TN TN TT TN TN TT N
+        // Part 2) i % 3 == 1: TN TN TT TN TN TT TN TN TT TN N
+        // Part 3) i % 3 == 2: TN TT TN TN TT TN TN TT TN TN N
+        for (int j = 0; j < 10; j++) {
+            if ((i+j) % 3 == 0) {
+                result += j;
+            }
+            result -= j;
+        }
     }
+    printf("Result: %d\n", result); // To avoid optimization out.
     return 0;
 }
